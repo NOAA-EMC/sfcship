51af1235bace7d47ef7c1d71f18f97d20822fca8
Steve Stegall <Steve.Stegall@noaa.gov>
2021-10-26 13:18:41 +0000

    Modified for WCOSS2 environment

--- util/sorc/datarch.fd/build.sh
+++ util/sorc/datarch.fd/build.sh
@@ -1,12 +1,13 @@
 set -ax
-module load ips/18.0.1.163  impi/18.0.1
-#module load ips/18.0.1.163  smpi/10.1.1.0
-module load graphics/2.0.0
-module load bufr/11.3.0
-module load w3nco/2.0.6
-module load bacio/2.0.2
-module use /gpfs/dell1/nco/ops/nwprod/modulefiles/mpi_prod/ips/18.0.1/impi/18.0.1/            
-#module use /gpfs/dell1/nco/ops/nwprod/modulefiles/mpi_prod/ips/18.0.1/impi/10.1.1.0/            
-module load w3emc/2.3.0
+module load envvar/1.0
+module load PrgEnv-intel/8.1.0
+module load craype/2.7.8
+module load intel/19.1.3.304
+module load cray-mpich/8.1.4
+#module load graphics/2.0.0
+module load bufr/11.4.0
+module load w3nco/2.4.1
+module load bacio/2.4.1
+module load w3emc/2.7.3
 module list
 make
