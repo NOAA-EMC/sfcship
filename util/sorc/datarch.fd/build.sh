edae081c0c5cf0a72dd118c2abd5f8f325d6fb33
Steve Stegall <Steve.Stegall@noaa.gov>
2021-10-28 19:21:22 +0000

    Modified for WCOSS2 environment

--- util/sorc/datarch.fd/build.sh
+++ util/sorc/datarch.fd/build.sh
@@ -4,10 +4,15 @@ module load PrgEnv-intel/8.1.0
 module load craype/2.7.8
 module load intel/19.1.3.304
 module load cray-mpich/8.1.4
+
+export VERROOT=/lfs/h2/emc/obsproc/noscrub/Steve.Stegall/githubwkspc/sfcship/versions
+. ${VERROOT}/build.ver
+
+module load w3nco/${w3nco_ver:?}
+module load bufr/${bufr_ver:?}
+module load w3emc/${w3emc_ver:?}
+module load bacio/${bacio_ver:?}
 #module load graphics/2.0.0
-module load bufr/11.4.0
-module load w3nco/2.4.1
-module load bacio/2.4.1
-module load w3emc/2.7.3
+
 module list
 make
