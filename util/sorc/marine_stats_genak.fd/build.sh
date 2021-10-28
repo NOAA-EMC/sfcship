82ba0dafebcf69ab175640279d51600412e280e9
Steve Stegall <Steve.Stegall@noaa.gov>
2021-10-28 19:22:31 +0000

    Modified for WCOSS2 environment

--- util/sorc/marine_stats_genak.fd/build.sh
+++ util/sorc/marine_stats_genak.fd/build.sh
@@ -1,4 +1,8 @@
 set -ax
-module load w3nco/2.4.1
+export VERROOT=/lfs/h2/emc/obsproc/noscrub/Steve.Stegall/githubwkspc/sfcship/versions
+. ${VERROOT}/build.ver
+
+module load w3nco/${w3nco_ver:?}
+
 module list
 make
